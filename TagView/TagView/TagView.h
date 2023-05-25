ba56bc723470b7862d73e5952aa3b881513f0b9c
Nico Prasasty S <nicoprasastysembiring@gmail.com>
2023-05-26 03:24:15 +0700

    common init

---
+++ TagView/TagView/TagView.h
@@ -0,0 +1,18 @@
+//
+//  TagView.h
+//  TagView
+//
+//  Created by Nico Prasasty S on 25/05/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TagView.
+FOUNDATION_EXPORT double TagViewVersionNumber;
+
+//! Project version string for TagView.
+FOUNDATION_EXPORT const unsigned char TagViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TagView/PublicHeader.h>
+
+
